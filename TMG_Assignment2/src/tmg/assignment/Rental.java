9aba3e1951857ca9b62fc56916614ee92485fa46
Griff2000 <82298628+Griff2000@users.noreply.github.com>
2021-04-14 18:22:37 +1200

    Add files via upload

---
+++ TMG_Assignment2/src/tmg/assignment/Rental.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tmg.assignment;
+
+import java.util.Date;
+
+/**
+ *
+ * @author grify
+ */
+public abstract class Rental {
+    private int rentalID;
+    private Date rentalDate;
+    
+}
