54d3560547ee24cc82d39a89d42fecc8e5ff2b2d
Mitchacleaver <Mitchacleaver@gmail.com>
2021-04-15 15:35:17 +1200

    TMGASSIGNMENT

--- TMG_Assignment2/src/tmg/assignment/IndividualGuitar.java
+++ TMG_Assignment2/src/tmg/assignment/IndividualGuitar.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package TMG.Assignment2;
+package tmg.assignment;
 
 import java.time.LocalDate;
 
